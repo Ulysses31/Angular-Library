332887ea1608a5ae1bd384b091574d4dfec0722a
ulysses31 <iordanidischr@gmail.com>
2022-01-22 09:23:47 +0200

    finished control value accessor

--- projects/corelib/src/lib/controls/ng-input-text-area/ng-input-text-area.component.ts
+++ projects/corelib/src/lib/controls/ng-input-text-area/ng-input-text-area.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { noop, Subject } from 'rxjs';
 
 @Component({
   selector: 'ng-input-text-area',
@@ -8,18 +9,58 @@ import { Subject } from 'rxjs';
       pInputTextarea
       rows="5"
       class="p-inputtext p-component noSize"
-      [name]="$any(name)"
-      [(ngModel)]="nmodel"
+      [ngClass]="{ 'inValid' : isValid }"
+      [(ngModel)]="value"
+      (blur)="onBlur()"
     >
     </textarea>
   `,
   styleUrls: ['./ng-input-text-area.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NgInputTextAreaComponent),
+      multi: true,
+    },
+  ],
 })
-export class NgInputTextAreaComponent implements OnInit {
-  @Input() name: string | undefined;
-  @Input() nmodel: string | undefined;
+export class NgInputTextAreaComponent implements OnInit, ControlValueAccessor {
+  @Input() isValid: boolean = false;
+  private innerValue: any = '';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+
+  get value(): any {
+    return this.innerValue;
+  }
+
+  set value(val: any) {
+    if(val !== this.innerValue) {
+      this.innerValue = val;
+      this.onChangeCallback(val);
+    }
+  }
+
+  onBlur(): void {
+    this.onTouchedCallback();
+  }
+
+  writeValue(value: any): void {
+    if(value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouchedCallback = fn;
+  }
 }
--- projects/corelib/src/lib/controls/ng-input-text/ng-input-text.component.ts
+++ projects/corelib/src/lib/controls/ng-input-text/ng-input-text.component.ts
@@ -1,5 +1,6 @@
-import { Subject } from 'rxjs';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { noop } from 'rxjs';
 
 @Component({
   selector: 'ng-input-text',
@@ -14,19 +15,59 @@ import { Component, Input, OnInit } from '@angular/core';
         type="text"
         pInputText
         class="p-inputtext p-component"
-        [name]="$any(name)"
-        [(ngModel)]="nmodel"
+        [ngClass]="{ 'inValid' : isValid }"
+        [(ngModel)]="value"
+        (blur)="onBlur()"
       />
     </span>
   `,
   styleUrls: ['./ng-input-text.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NgInputTextComponent),
+      multi: true,
+    },
+  ],
 })
-export class NgInputTextComponent implements OnInit {
-  @Input() name: string | undefined;
-  @Input() nmodel: string | undefined;
+export class NgInputTextComponent implements OnInit, ControlValueAccessor {
   @Input() isBusy: boolean = false;
+  @Input() isValid: boolean = false;
+  private innerValue: any = '';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+
+  get value(): any {
+    return this.innerValue;
+  }
+
+  set value(val: any) {
+    if(val !== this.innerValue) {
+      this.innerValue = val;
+      this.onChangeCallback(val);
+    }
+  }
+
+  onBlur(): void {
+    this.onTouchedCallback();
+  }
+
+  writeValue(value: any): void {
+    if(value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouchedCallback = fn;
+  }
 }
